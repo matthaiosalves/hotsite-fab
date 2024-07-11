e2cd8d9ce5626d1348fb152aea7b4c3408e6ceec
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-07-11 00:55:10 -0300

    :sparkles: Importado novos componentes no perfil

--- src/views/PerfilView.vue
+++ src/views/PerfilView.vue
@@ -1,5 +1,8 @@
 <script setup>
 import Perfil from "@/components/Perfil.vue";
+import AcessoRapido from "@/components/AcessoRapido.vue";
+import PracaDestaque from "@/components/PracaDestaque.vue";
+import OficialDestaque from "@/components/OficialDestaque.vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
@@ -13,6 +16,11 @@ const user = route.params.user;
         <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-8 col-xl-8">
             <Perfil :user="user" />
+          </div>
+          <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+            <PracaDestaque />
+            <OficialDestaque />
+            <AcessoRapido />
           </div>
         </div>
       </div>
