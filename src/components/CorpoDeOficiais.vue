6ce45b5b28432b728a4de068313c528bd596be06
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-07-12 00:09:17 -0300

    :fix Ajuste no fetch

--- src/components/CorpoDeOficiais.vue
+++ src/components/CorpoDeOficiais.vue
@@ -36,22 +36,40 @@
 export default {
   name: "CorpoDeOficiais",
   data() {
+    const BASE_URL =
+      "https://www.habbo.com.br/habbo-imaging/avatarimage?&action=sit&direction=2&head_direction=3&img_format=gif&gesture=sml&frame=1&headonly=0&size=m";
+
+    const getImageUrl = (userName) => `${BASE_URL}&user=${userName}`;
+
     return {
-      BASE_URL:
-        "https://www.habbo.com.br/habbo-imaging/avatarimage?&action=sit&direction=2&head_direction=3&img_format=gif&gesture=sml&frame=1&headonly=0&size=m",
-      users: [],
       rankGroups: [],
+      getImageUrl,
     };
   },
   methods: {
     async fetchUsers() {
       try {
         const response = await fetch("https://fabhabbo.com/fetch/oficiais");
+
+        // Verifique se a resposta está OK (status 200-299)
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
         const fetchedUsers = await response.json();
+
+        // Verifique a estrutura dos dados recebidos
+        if (!Array.isArray(fetchedUsers)) {
+          throw new Error("Os dados recebidos não são um array");
+        }
+
+        // Mapeia os dados recebidos para o formato desejado
         const mappedUsers = fetchedUsers.map((user) => ({
           name: user.nome,
-          rank: this.mapRank(user.cargo_id),
+          rank: this.mapRank(user.cargo_id), // Supondo que há um método para mapear os cargos
         }));
+
+        // Agrupa os usuários por rank
         this.rankGroups = [
           {
             rank: "Generais",
@@ -83,7 +101,8 @@ export default {
       }
     },
     mapRank(cargo_id) {
-      const ranks = {
+      // Função de mapeamento do cargo_id para o rank (exemplo)
+      const rankMap = {
         1: "Marechal-do-Ar ★★★★★",
         2: "Tenente-Brigadeiro ★★★★",
         3: "Major",
@@ -92,13 +111,10 @@ export default {
         6: "Segundo Tenente",
         7: "Aspirante-a-Oficial",
       };
-      return ranks[cargo_id] || "Desconhecido";
-    },
-    getImageUrl(userName) {
-      return `${this.BASE_URL}&user=${userName}`;
+      return rankMap[cargo_id] || "Desconhecido";
     },
   },
-  created() {
+  mounted() {
     this.fetchUsers();
   },
 };
