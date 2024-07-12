9982341ff831843eea6a56c141c4bf7e0b0532e2
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-07-12 00:15:30 -0300

    :fix retornado o mock

--- src/components/CorpoDeOficiais.vue
+++ src/components/CorpoDeOficiais.vue
@@ -39,83 +39,95 @@ export default {
     const BASE_URL =
       "https://www.habbo.com.br/habbo-imaging/avatarimage?&action=sit&direction=2&head_direction=3&img_format=gif&gesture=sml&frame=1&headonly=0&size=m";
 
+    const users = [
+      {
+        name: "Alex-Pearce",
+        rank: "Marechal-do-Ar ★★★★★",
+      },
+      {
+        name: "::Yashida",
+        rank: "Tenente-Brigadeiro ★★★★",
+      },
+      {
+        name: "LonaaCitan",
+        rank: "Major",
+      },
+      {
+        name: "Mascarenha",
+        rank: "Major",
+      },
+      {
+        name: "Zenitsu_l",
+        rank: "Major",
+      },
+      {
+        name: "felipeblu",
+        rank: "Capitão",
+      },
+      {
+        name: ".Apollo!",
+        rank: "Primeiro Tenente",
+      },
+      {
+        name: "barkk",
+        rank: "Primeiro Tenente",
+      },
+      {
+        name: "luizbethove",
+        rank: "Segundo Tenente",
+      },
+      {
+        name: "VIIPAZO",
+        rank: "Aspirante-a-Oficial",
+      },
+      {
+        name: "Eficientemente",
+        rank: "Aspirante-a-Oficial",
+      },
+      {
+        name: "danieljhim9",
+        rank: "Aspirante-a-Oficial",
+      },
+      {
+        name: "wosk",
+        rank: "Aspirante-a-Oficial",
+      },
+    ];
+
     const getImageUrl = (userName) => `${BASE_URL}&user=${userName}`;
 
+    const rankGroups = [
+      {
+        rank: "Generais",
+        class: "generais",
+        users: users.filter(
+          (user) => user.rank.includes("Marechal") || user.rank.includes("Brigadeiro")
+        ),
+      },
+      {
+        rank: "Superiores",
+        class: "superiores",
+        users: users.filter((user) => user.rank.includes("Major")),
+      },
+      {
+        rank: "Intermediários",
+        class: "intermediarios",
+        users: users.filter((user) => user.rank.includes("Capitão")),
+      },
+      {
+        rank: "Subalternos",
+        class: "subalternos",
+        users: users.filter(
+          (user) => user.rank.includes("Tenente") || user.rank.includes("Aspirante")
+        ),
+      },
+    ];
+
     return {
-      rankGroups: [],
+      users,
       getImageUrl,
+      rankGroups,
     };
-  },
-  methods: {
-    async fetchUsers() {
-      try {
-        const response = await fetch("https://fabhabbo.com/fetch/oficiais");
-
-        // Verifique se a resposta está OK (status 200-299)
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const fetchedUsers = await response.json();
-
-        // Verifique a estrutura dos dados recebidos
-        if (!Array.isArray(fetchedUsers)) {
-          throw new Error("Os dados recebidos não são um array");
-        }
-
-        // Mapeia os dados recebidos para o formato desejado
-        const mappedUsers = fetchedUsers.map((user) => ({
-          name: user.nome,
-          rank: this.mapRank(user.cargo_id), // Supondo que há um método para mapear os cargos
-        }));
-
-        // Agrupa os usuários por rank
-        this.rankGroups = [
-          {
-            rank: "Generais",
-            class: "generais",
-            users: mappedUsers.filter(
-              (user) => user.rank.includes("Marechal") || user.rank.includes("Brigadeiro")
-            ),
-          },
-          {
-            rank: "Superiores",
-            class: "superiores",
-            users: mappedUsers.filter((user) => user.rank.includes("Major")),
-          },
-          {
-            rank: "Intermediários",
-            class: "intermediarios",
-            users: mappedUsers.filter((user) => user.rank.includes("Capitão")),
-          },
-          {
-            rank: "Subalternos",
-            class: "subalternos",
-            users: mappedUsers.filter(
-              (user) => user.rank.includes("Tenente") || user.rank.includes("Aspirante")
-            ),
-          },
-        ];
-      } catch (error) {
-        console.error("Erro ao buscar usuários:", error);
-      }
-    },
-    mapRank(cargo_id) {
-      // Função de mapeamento do cargo_id para o rank (exemplo)
-      const rankMap = {
-        1: "Marechal-do-Ar ★★★★★",
-        2: "Tenente-Brigadeiro ★★★★",
-        3: "Major",
-        4: "Capitão",
-        5: "Primeiro Tenente",
-        6: "Segundo Tenente",
-        7: "Aspirante-a-Oficial",
-      };
-      return rankMap[cargo_id] || "Desconhecido";
-    },
-  },
-  mounted() {
-    this.fetchUsers();
   },
 };
 </script>
