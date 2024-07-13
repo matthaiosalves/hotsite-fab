14169381e29d579f636159af38b5633f37f5123b
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-07-13 15:20:46 -0300

    :sparkles: Implementado logica no array pra receber width e height

--- src/components/AcessoRapido.vue
+++ src/components/AcessoRapido.vue
@@ -10,8 +10,8 @@
             <img
               :src="item.image"
               :alt="item.text"
-              width="auto"
-              height="auto"
+              :width="item.width"
+              :height="item.height"
               lazy="loading"
             />
             <span>{{ item.text }}</span>
@@ -20,8 +20,8 @@
             <img
               :src="item.image"
               :alt="item.text"
-              width="auto"
-              height="auto"
+              :width="item.width"
+              :height="item.height"
               lazy="loading"
             />
             <span>{{ item.text }}</span>
@@ -42,23 +42,31 @@ export default {
           link: "/instituicao/fardamentos",
           image: "https://www.fabhabbo.com/public/images/icons/uniform.png",
           text: "Fardamentos",
+          width: "40",
+          height: "52",
         },
         {
           link: "/apostilas/peles-e-cabelos",
           image: "https://www.fabhabbo.com/public/images/icons/peles.png",
           text: "Peles e cabelos",
+          width: "40",
+          height: "40",
         },
         {
           link: "#",
           image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjv8bd1tiv_f6KCyEtq5QEB4GWM1rsPIjlY9RboA-miz22V4uM5WUM6J9QDO7sq2-0z_qZ2FH3Gqbq1bJDKV8yp8zTthPMRW1pR3fY2OkpfnrT_IYYiSyid77fxlvi2agiPhkIXmhqLGJXB/s1600/FR09A.gif",
           text: "Hierarquia Militar",
+          width: "40",
+          height: "40",
         },
         {
           link: "#",
           image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiQrhMlmV231lS074OnvP4LiAgJgzbbO4GfGnmXmTBP9T8Z_3vF66WFeGIsgaiodg1FtNww326LaKNtGBp3icih82IHt5rjiw8PqKwmAKtPta9YyvLH6GEn9tntQNHD_dcrqlO0SJh5y7I/s1600/ARM28.gif",
           text: "Academias",
+          width: "40",
+          height: "40",
         },
       ],
     };
