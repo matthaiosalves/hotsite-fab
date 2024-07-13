dee34b2e5c14e4e5f42645199c45ac7cf02cc0d9
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-07-12 23:00:59 -0300

    :sparkles: Instalado o axios e puxando da api os Oficiais

--- src/components/CorpoDeOficiais.vue
+++ src/components/CorpoDeOficiais.vue
@@ -33,101 +33,67 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "CorpoDeOficiais",
   data() {
-    const BASE_URL =
-      "https://www.habbo.com.br/habbo-imaging/avatarimage?&action=sit&direction=2&head_direction=3&img_format=gif&gesture=sml&frame=1&headonly=0&size=m";
-
-    const users = [
-      {
-        name: "Alex-Pearce",
-        rank: "Marechal-do-Ar ★★★★★",
-      },
-      {
-        name: "::Yashida",
-        rank: "Tenente-Brigadeiro ★★★★",
-      },
-      {
-        name: "LonaaCitan",
-        rank: "Major",
-      },
-      {
-        name: "Mascarenha",
-        rank: "Major",
-      },
-      {
-        name: "Zenitsu_l",
-        rank: "Major",
-      },
-      {
-        name: "felipeblu",
-        rank: "Capitão",
-      },
-      {
-        name: ".Apollo!",
-        rank: "Primeiro Tenente",
-      },
-      {
-        name: "barkk",
-        rank: "Primeiro Tenente",
-      },
-      {
-        name: "luizbethove",
-        rank: "Segundo Tenente",
-      },
-      {
-        name: "VIIPAZO",
-        rank: "Aspirante-a-Oficial",
-      },
-      {
-        name: "Eficientemente",
-        rank: "Aspirante-a-Oficial",
-      },
-      {
-        name: "danieljhim9",
-        rank: "Aspirante-a-Oficial",
-      },
-      {
-        name: "wosk",
-        rank: "Aspirante-a-Oficial",
-      },
-    ];
-
-    const getImageUrl = (userName) => `${BASE_URL}&user=${userName}`;
-
-    const rankGroups = [
-      {
-        rank: "Generais",
-        class: "generais",
-        users: users.filter(
-          (user) => user.rank.includes("Marechal") || user.rank.includes("Brigadeiro")
-        ),
-      },
-      {
-        rank: "Superiores",
-        class: "superiores",
-        users: users.filter((user) => user.rank.includes("Major")),
-      },
-      {
-        rank: "Intermediários",
-        class: "intermediarios",
-        users: users.filter((user) => user.rank.includes("Capitão")),
-      },
-      {
-        rank: "Subalternos",
-        class: "subalternos",
-        users: users.filter(
-          (user) => user.rank.includes("Tenente") || user.rank.includes("Aspirante")
-        ),
-      },
-    ];
-
     return {
-      users,
-      getImageUrl,
-      rankGroups,
+      users: [],
+      rankGroups: [],
+      BASE_URL:
+        "https://www.habbo.com.br/habbo-imaging/avatarimage?&action=sit&direction=2&head_direction=3&img_format=gif&gesture=sml&frame=1&headonly=0&size=m",
     };
+  },
+  methods: {
+    getImageUrl(userName) {
+      return `${this.BASE_URL}&user=${userName}`;
+    },
+    fetchUsers() {
+      axios
+        .get("https://fabhabbo.com/fetch/oficiais")
+        .then((response) => {
+          this.users = response.data.success.map((item) => ({
+            name: item.alistado_nome,
+            rank: item.patente_nome,
+          }));
+          this.groupUsersByRank();
+        })
+        .catch((error) => {
+          console.error("Erro ao buscar os dados:", error);
+        });
+    },
+    groupUsersByRank() {
+      this.rankGroups = [
+        {
+          rank: "Generais",
+          class: "generais",
+          users: this.users.filter(
+            (user) => user.rank.includes("Marechal") || user.rank.includes("Brigadeiro")
+          ),
+        },
+        {
+          rank: "Superiores",
+          class: "superiores",
+          users: this.users.filter((user) => user.rank.includes("Major")),
+        },
+        {
+          rank: "Intermediários",
+          class: "intermediarios",
+          users: this.users.filter((user) => user.rank.includes("Capitão")),
+        },
+        {
+          rank: "Subalternos",
+          class: "subalternos",
+          users: this.users.filter(
+            (user) => user.rank.includes("Tenente") || user.rank.includes("Aspirante")
+          ),
+        },
+      ];
+    },
+  },
+  created() {
+    this.fetchUsers();
   },
 };
 </script>
