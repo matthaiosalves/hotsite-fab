7470664e8ceebeefae916306637fe6a441e156ce
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-07-13 15:23:11 -0300

    :bug: Ajustado os textos do alt das imagens

--- src/components/AcessoRapido.vue
+++ src/components/AcessoRapido.vue
@@ -41,14 +41,14 @@ export default {
         {
           link: "/instituicao/fardamentos",
           image: "https://www.fabhabbo.com/public/images/icons/uniform.png",
-          text: "Fardamentos",
+          text: "Icone que representa um fardamento no lado esquerdo.",
           width: "40",
           height: "52",
         },
         {
           link: "/apostilas/peles-e-cabelos",
           image: "https://www.fabhabbo.com/public/images/icons/peles.png",
-          text: "Peles e cabelos",
+          text: "Icone de uma cara que representa peles e cabelos no lado esquerdo.",
           width: "40",
           height: "40",
         },
@@ -56,7 +56,8 @@ export default {
           link: "#",
           image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjv8bd1tiv_f6KCyEtq5QEB4GWM1rsPIjlY9RboA-miz22V4uM5WUM6J9QDO7sq2-0z_qZ2FH3Gqbq1bJDKV8yp8zTthPMRW1pR3fY2OkpfnrT_IYYiSyid77fxlvi2agiPhkIXmhqLGJXB/s1600/FR09A.gif",
-          text: "Hierarquia Militar",
+          text:
+            "Icone de um brasão para representar a hierarquia militar no lado esquerdo.",
           width: "40",
           height: "40",
         },
@@ -64,7 +65,8 @@ export default {
           link: "#",
           image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiQrhMlmV231lS074OnvP4LiAgJgzbbO4GfGnmXmTBP9T8Z_3vF66WFeGIsgaiodg1FtNww326LaKNtGBp3icih82IHt5rjiw8PqKwmAKtPta9YyvLH6GEn9tntQNHD_dcrqlO0SJh5y7I/s1600/ARM28.gif",
-          text: "Academias",
+          text:
+            "Icone de um militar em um campo de instrução para representar as academias no lado esquerdo.",
           width: "40",
           height: "40",
         },
